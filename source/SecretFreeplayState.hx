7fdd502137c49da03af4570666f3f148b99bc1ba
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-06-21 10:30:35 -0400

    Update SecretFreeplayState.hx

--- source/SecretFreeplayState.hx
+++ source/SecretFreeplayState.hx
@@ -19,7 +19,7 @@ import flixel.system.FlxSound;
 import openfl.utils.Assets as OpenFlAssets;
 import SecretWeekState;
 import FreeplayState;
-import FreeplayState.SongMetadata‎;
+import FreeplayState.SongMetadata;
 #if MODS_ALLOWED
 import sys.FileSystem;
 #end
